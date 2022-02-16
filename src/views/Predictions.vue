520ea5218f4ee09f94cd3ab52950716a57ad26e1
Alex Brady <apb121@texel32.doc.ic.ac.uk>
2022-02-16 12:34:00 +0000

    another attempt

--- src/views/Predictions.vue
+++ src/views/Predictions.vue
@@ -3,33 +3,27 @@
     <h1>This is an about page</h1>
   </div>
 </template>
-
-<body>
-    <div class="canvas">
-    </div>
-    <!-- Link with firebase data -->
     
-    <script src="https://www.gstatic.com/firebasejs/9.0.2/firebase-app-compat.js"></script> 
-    <script src="https://www.gstatic.com/firebasejs/9.0.2/firebase-firestore-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.0.2/firebase-app-compat.js"></script> 
+<script src="https://www.gstatic.com/firebasejs/9.0.2/firebase-firestore-compat.js"></script>
     
-    <script type="module"></script>
-        <script>
-        // Firebase web app configuration
-        const firebaseConfig = {
-          apiKey: "AIzaSyCMbegkc1LAvlUpj2akUiBr_I9lB2OW19k",
-          authDomain: "practice-firebase-52292.firebaseapp.com",
-          projectId: "practice-firebase-52292",
-          storageBucket: "practice-firebase-52292.appspot.com",
-          messagingSenderId: "481429582032",
-          appId: "1:481429582032:web:de4d582b032c9d8ad6eed5",
-          measurementId: "G-QBXC1HV2C0"
-        };
+<script type="module"></script>
+    <script>
+    // Firebase web app configuration
+    const firebaseConfig = {
+      apiKey: "AIzaSyCMbegkc1LAvlUpj2akUiBr_I9lB2OW19k",
+      authDomain: "practice-firebase-52292.firebaseapp.com",
+      projectId: "practice-firebase-52292",
+      storageBucket: "practice-firebase-52292.appspot.com",
+      messagingSenderId: "481429582032",
+      appId: "1:481429582032:web:de4d582b032c9d8ad6eed5",
+      measurementId: "G-QBXC1HV2C0"
+    };
 
-        //Initialize Firebase
-        firebase.initializeApp(firebaseConfig);
-        const db = firebase.firestore();
+    //Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    const db = firebase.firestore();
 
-    </script>
-    <script src="https://d3js.org/d3.v7.min.js"></script> 
-    <script src="index.js"></script>
-</body>
+</script>
+<script src="https://d3js.org/d3.v7.min.js"></script> 
+<script src="firebase_index.js"></script>
