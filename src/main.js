8c091d095c399ed9359ba0fef476e6d122769447
Livia <lgf21@doc.ic.ac.uk>
2022-02-16 18:36:05 +0000

    shot in the dark

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import * as d3 from 'd3'
+import * as firebase from 'firebase'
+import * as Charts from './charts/charts.js'
 
 createApp(App).use(router).mount('#app')
