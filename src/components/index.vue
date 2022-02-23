28d7a9b558d4fb62d6c9115aa3c7d677b1d49e3c
Patrick Moran <patrick.moran1998@yahoo.co.uk>
2022-02-23 19:07:19 +0000

    our last effort

--- src/components/index.vue
+++ src/components/index.vue
@@ -14,12 +14,18 @@
   export default
   {
       mounted: function() {
-            cred = credentials.Certificate('practice_key.json')
-            firebase_admin.initialize_app(cred, 
-            {
-                'databaseURL': "https://practice-firebase-52292-default-rtdb.europe-west1.firebasedatabase.app/"
-            })
-            const db = getFirestore();
+            const firebaseConfig = {
+                apiKey: "AIzaSyCMbegkc1LAvlUpj2akUiBr_I9lB2OW19k",
+                authDomain: "practice-firebase-52292.firebaseapp.com",
+                databaseURL: "https://practice-firebase-52292-default-rtdb.europe-west1.firebasedatabase.app",
+                projectId: "practice-firebase-52292",
+                storageBucket: "practice-firebase-52292.appspot.com",
+                messagingSenderId: "481429582032",
+                appId: "1:481429582032:web:de4d582b032c9d8ad6eed5",
+                measurementId: "G-QBXC1HV2C0"
+            };
+            const app = initializeApp(firebaseConfig);
+            const db = getDatabase();
 
             const svg = d3.select("#starterChart")
             .attr('width', 700)
