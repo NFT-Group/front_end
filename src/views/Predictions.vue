57da03251b629f99b56453f9bc328c33a5b4eff0
Alex Brady <apb121@sprite03.doc.ic.ac.uk>
2022-03-06 16:39:43 +0000

    api 1

--- src/views/Predictions.vue
+++ src/views/Predictions.vue
@@ -15,6 +15,35 @@
   <br><br><br><br>
 </template>
 
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'Predictions',
+  data() {
+    return {
+      msg: '',
+    };
+  },
+  methods: {
+    getMessage() {
+      const path = 'https://front-end-one-smoky.vercel.app/predictions';
+      axios.get(path)
+        .then((res) => {
+          this.msg = res.data;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.error(error);
+        });
+    },
+  },
+  created() {
+    this.getMessage();
+  },
+};
+</script>
+
 
 <style>
   h2{
