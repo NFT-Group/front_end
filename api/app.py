b0df971464aee3a35c891599338ff9b32f130bc6
Alex Brady <apb121@gpu22.doc.ic.ac.uk>
2022-03-07 15:47:32 +0000

    god damn why was it that

--- api/app.py
+++ api/app.py
@@ -1,13 +1,13 @@
 from flask import Flask, Response, jsonify
-#from random import randint
+from random import randint
 
 app = Flask(__name__)
 
 #@app.route('/', defaults={'path': ''})
 @app.route('/api/get_price', methods=["GET"])
 def api():
-    #price = randint(1000000, 10000000)
-    response_json = {"price" : "One squillion billion pounds!"}
+    price = randint(1000000, 10000000)
+    response_json = {"price" : "That NFT would cost " + str(price) + "pounds! Wow!"}
     return jsonify(response_json)
     #if request.method == 'GET':
         #response_object = {"price": "1000"}
