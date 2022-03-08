f20d44e6d438ab14ebcc21d5e7153296604dc338
Alex Brady <apb121@vm-shell1.doc.ic.ac.uk>
2022-03-08 23:12:41 +0000

    checked checker but better

--- src/views/Predictions.vue
+++ src/views/Predictions.vue
@@ -37,8 +37,8 @@ export default {
   methods: {
     onSubmit(evt) {
       console.log(evt);
-      console.log("is cryptoadz checked?")
-      console.log(evt.target.elements["cryptoad"])
+      console.log("which element is checked?")
+      console.log(evt.srcElement.collection.value)
       evt.preventDefault();
       console.log("entering onSubmit")
       const path = 'https://front-end-one-smoky.vercel.app/api/get_price';
