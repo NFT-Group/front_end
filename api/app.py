f1e84fe34984bf9826f9891d526834b38709d4aa
Tom Mitcheson <tm21@sprite16.doc.ic.ac.uk>
2022-03-09 14:55:01 +0000

    Practice machine learning algorithm

--- api/app.py
+++ api/app.py
@@ -7,10 +7,13 @@ app = Flask(__name__)
 @app.route('/api/get_price', methods=["POST"])
 def api():
     collection = request.data
-    price = randint(100, 10000000)
+    price = machine_learning(10) #randint(100, 10000000)
     response_json = {"price" : "That " + str(collection)[2:-1] + " would cost £" + str(price) + "! Wow!"}
     return jsonify(response_json)
     #if request.method == 'GET':
         #response_object = {"price": "1000"}
         #return jsonify(response_object)
     #return Response("<h1>Flask</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
+
+def machine_learning(a):
+    return a+1
