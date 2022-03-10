e7163fad539c4abd64fe934ae18b2ea67c294999
Alex Brady <apb121@vertex53.doc.ic.ac.uk>
2022-03-10 15:13:40 +0000

    hmm

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -86,7 +86,7 @@ export default {
             .attr('fill', '#2c3e50')
             .style('font-weight', 600);
 
-        const reference = ref(db, '/');
+        //const reference = ref(db, '/');
 
         var apeAddress = '0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D'
         var cryptoPunkAddress = '0xb47e3cd837dDF8e4c57F05d70Ab865de6e193BBB'
@@ -96,6 +96,8 @@ export default {
         var crypToadzAddress = '0x1CB1A5e65610AEFF2551A50f76a87a7d3fB649C6'
         var boredApeKennelAddress = '0xba30E5F9Bb24caa003E9f2f0497Ad287FDF95623'
         var pudgyPenguinAddress = '0xBd3531dA5CF5857e7CfAA92426877b022e612cf8'
+
+        var reference = db.ref("/");
 
         reference.orderByChild('timestamp').startAt('2022-02-10').on('value', (snapshot) => {
             const data = Object.values(snapshot.val())
