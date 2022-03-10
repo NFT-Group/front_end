5e11590fc5c4c4b2f34fb210fdcb26030068e288
Tom Mitcheson <tm21@sprite16.doc.ic.ac.uk>
2022-03-10 18:27:48 +0000

    Function for returning predicted price and corresponding keys in place. Not functional yet.

--- api/app.py
+++ api/app.py
@@ -3,6 +3,36 @@ from random import randint
 
 app = Flask(__name__)
 
+# CREATE LINK FOR 'FULL DATABASE'
+
+cred_push_key = str(pathlib.Path(__file__).parent.resolve()) + '/database_store_keys/key_for_ML-prepped-database.json'
+cred_push = firebase_admin.credentials.Certificate(cred_push_key)
+default_app = firebase_admin.initialize_app(cred_push, {
+    'databaseURL':'https://ml-prepped-database-default-rtdb.europe-west1.firebasedatabase.app/'
+    })
+
+
+def find_price_predictor_from_tokenid(collection_name, tokenid):
+    # find model
+    filename = collection_name + "_model.pkl"
+    loaded_model = pickle.load(open(filename, 'rb'))
+
+    # find input
+    nft_string = collection_name+tokenid
+    ref = db.reference(nft_string)
+    data_for_input = ref.get()
+
+    # format input 
+    data_for_input_json = pd.DataFrame([data_for_input])
+    data_for_input_json = data_for_input_json.drop(['NameOfCollection', 'ethprice', axis=1])
+    data_for_input_json['timestamp'] = 0
+
+    predicted_price = loaded_model.predict(data_for_input_json)
+    return predicted_price
+
+
+        
+
 #@app.route('/', defaults={'path': ''})
 @app.route('/api/get_price', methods=["POST"])
 def api():
