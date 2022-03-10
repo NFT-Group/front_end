4ff997537c5171981ca170d1e028f8f4927aa8fc
Tom Mitcheson <tm21@sprite16.doc.ic.ac.uk>
2022-03-10 18:54:01 +0000

    More fixes, please work

--- api/app.py
+++ api/app.py
@@ -54,7 +54,7 @@ def find_price_predictor_from_tokenid(request):
 
     # format input 
     data_for_input_json = pd.DataFrame([data_for_input])
-    data_for_input_json = data_for_input_json.drop(['NameOfCollection', 'ethprice'], axis=1])
+    data_for_input_json = data_for_input_json.drop(['NameOfCollection', 'ethprice'], axis=1)
     data_for_input_json['timestamp'] = 0
 
     predicted_price = loaded_model.predict(data_for_input_json)
