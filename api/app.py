8f4075c37769a4cc569c59c73cdda0c4e900e2e0
Tom Mitcheson <tm21@texel32.doc.ic.ac.uk>
2022-03-11 11:42:33 +0000

    see if builds all within one file

--- api/app.py
+++ api/app.py
@@ -1,12 +1,12 @@
 from flask import Flask, Response, jsonify, request
-from random import randint
+# from random import randint
 import time
 from datetime import datetime
 import firebase_admin
 from firebase_admin import credentials, firestore, db
 import json
-# import pickle
-#import sklearn
+import pickle
+import sklearn
 from front_end.public.node_graph_data.helper_functions import find_price_predictor_from_tokenid
 # import sys
 # sys.path.insert(1, '/public/node_graph_data/')
@@ -57,20 +57,42 @@ transactions_app = None
 #@app.route('/', defaults={'path': ''})
 @app.route('/api/get_price', methods=["POST"])
 def api():
-    #cred_push_key = str(pathlib.Path(__file__).parent.resolve()) + '/database_store_keys/key_for_ML-prepped-database.json'
-    # cred_push = firebase_admin.credentials.Certificate(cred_push_key)
-    # try:
-    #     firebase_admin.delete_app(transactions_app)
-    # except:
-    #     a = cred_push # dummy operation
-    # try:
-    #     ml_app = firebase_admin.initialize_app(cred_push, { 'databaseURL':'https://ml-prepped-database-default-rtdb.europe-west1.firebasedatabase.app/' } )
-    # except:
-    #     a = cred_push # dummy operation
+    # cred_push_key = str(pathlib.Path(__file__).parent.resolve()) + '/database_store_keys/key_for_ML-prepped-database.json'
+    cred_push = firebase_admin.credentials.Certificate(cred_push_key)
+    try:
+        firebase_admin.delete_app(transactions_app)
+    except:
+        a = cred_push # dummy operation
+    try:
+        ml_app = firebase_admin.initialize_app(cred_push, { 'databaseURL':'https://ml-prepped-database-default-rtdb.europe-west1.firebasedatabase.app/' } )
+    except:
+        a = cred_push # dummy operation
     collection = json.loads(str(request.data)[2:-1])
-    price = find_price_predictor_from_tokenid(collection) #randint(100, 10000000)
+
+    # process request
+    collection_name = request['collection']
+    tokenID = request['tokenid']
+
+    # find model
+    filename = "../node_graph_data/" + collection_name + "_model.pkl"
+
+    loaded_model = pickle.load(open(filename, 'rb'))
+
+    # find input
+    nft_string = collection_name+tokenID
+    ref = db.reference(nft_string)
+    data_for_input = ref.get()
+
+    # format input 
+    data_for_input_json = pd.DataFrame([data_for_input])
+    data_for_input_json = data_for_input_json.drop(['NameOfCollection', 'ethprice'], axis=1)
+    data_for_input_json['timestamp'] = 0
+
+    price = loaded_model.predict(data_for_input_json)
+
+    # price = find_price_predictor_from_tokenid(collection) #randint(100, 10000000)
     response_json = {"price" : "That " + str(collection)[2:-1] + " would cost £" + str(price) + "! Wow!"}
-    # firebase_admin.delete_app(ml_app) # there will DEFINITELY be a better way of doing this!!
+    firebase_admin.delete_app(ml_app) # there will DEFINITELY be a better way of doing this!!
     return jsonify(response_json)
     #if request.method == 'GET':
         #response_object = {"price": "1000"}
